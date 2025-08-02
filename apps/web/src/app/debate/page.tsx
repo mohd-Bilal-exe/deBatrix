c4f2182642740c16617bf6a9828a0697f2e20090
Mohd Bilal <bilalmail7054@gmail.com>
2025-08-02 14:33:55 +0530

    more fix

--- apps/web/src/app/debate/page.tsx
+++ apps/web/src/app/debate/page.tsx
@@ -16,7 +16,13 @@ export default function Debatepage() {
 
 useEffect(() => {
     const lenis = new Lenis({
-      lerp: 0.06,
+      lerp: 0.06,duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
     });
   
     function raf(time:number) {
